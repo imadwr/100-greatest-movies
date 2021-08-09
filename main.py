24195a7995328c962548104843f5de31986268ca
Imad Ouairem <67003840+imadwr@users.noreply.github.com>
2021-08-10 00:57:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(website, "html.parser")
 all_movies = soup.find_all(name="h3", class_="_h3_cuogz_1")
 movies_titles = [movie.getText().replace("\xa0", " ") for movie in all_movies]
 
-with open("movies.txt", "a") as file:
+with open("movies.txt", "w") as file:
     for movie in movies_titles:
         file.write(movie + "\n")
 
